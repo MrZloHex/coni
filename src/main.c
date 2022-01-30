214e0a68fc471c6c83dcc2416154190075274779
MrZloHex <zlo.alex.it@gmail.com>
2022-01-30 23:24:50 +0200

    CHATTING

--- src/main.c
+++ src/main.c
@@ -18,45 +18,43 @@ main
 
 	uint8_t nt = get_node_type(argc, argv);
 
-	int me;
+	int conn;
 
 
 	if (nt == SERVER_NT)
 	{
 		uint16_t port = get_port(argc, argv);
-		me = start_tcp(SERVER, NULL, port);
+		conn = start_tcp(SERVER, NULL, port);
 	}
 	else if (nt == CLIENT_NT)
 	{
 		char ip[16] = {0};
 		uint16_t port = get_ip_port(argc, argv, ip);
-		me = start_tcp(CLIENT, ip, port);
+		conn = start_tcp(CLIENT, ip, port);
 	}
 
 
 	while (M_PI)
+	{
+		cmd_T cmd = get_cmd();
+		if (cmd.type == EXIT)
 		{
-			cmd_T cmd = get_cmd();
-			if (cmd.type == EXIT)
-			{
-				printf("SERVER OFF\n");
-				stop_tcp(me);
-				exit(0);
-			}
-			else if (cmd.type == SEND)
-			{
-				printf("Sending messsage `%s`\n", cmd.data);
-				send_tcp(me, cmd.data, 20);
-			}
-			else if (cmd.type == LISTEN)
-			{
-				printf("Listening...\n");
-				char buf[200] = {0};
-				listen_tcp(me, buf, 200);
-				printf("GOT A MSG: `%s`\n", buf);
-			}
+			printf("SERVER OFF\n");
+			stop_tcp(conn);
+			exit(0);
 		}
-
-
+		else if (cmd.type == SEND)
+		{
+			printf("Sending messsage `%s`\n", cmd.data);
+			send_tcp(conn, cmd.data, 20);
+		}
+		else if (cmd.type == LISTEN)
+		{
+			printf("Listening...\n");
+			char buf[200] = {0};
+			listen_tcp(conn, buf, 200);
+			printf("GOT A MSG: `%s`\n", buf);
+		}
+	}
 	return 0;
 }
--- src/tcp.c
+++ src/tcp.c
@@ -10,107 +10,106 @@
 int
 start_tcp
 (
-    int type,
+	int type,
 	const char *ip,
 	uint16_t port
 )
 {
-    if (type == SERVER)
-    {
-	    printf("STARTING SERVER ON PORT %hu\n", port);
+	if (type == SERVER)
+	{
+		printf("STARTING SERVER ON PORT %hu\n", port);
 
-	    int sock_dscr;
-	    struct sockaddr_in server;
-	    struct sockaddr_in client;
-	    socklen_t namelen = sizeof(client);
-	    int ns;
+	    	int sock_dscr;
+	    	struct sockaddr_in server;
+	    	struct sockaddr_in client;
+		socklen_t namelen = sizeof(client);
+	    	int ns;
 
-	    sock_dscr = socket(AF_INET, SOCK_STREAM, 0);
-	    if (sock_dscr == -1)
-	    {
-	    	fprintf(stderr, "ERROR: failed to create a socket\n");
-	    	exit(1);
-	    }
+	    	sock_dscr = socket(AF_INET, SOCK_STREAM, 0);
+	    	if (sock_dscr == -1)
+	    	{
+			fprintf(stderr, "ERROR: failed to create a socket\n");
+			exit(1);
+	    	}
 
-	    server.sin_family      = AF_INET;
-	    server.sin_port        = htons(port);
-	    server.sin_addr.s_addr = INADDR_ANY;
+	    	server.sin_family      = AF_INET;
+	    	server.sin_port        = htons(port);
+	    	server.sin_addr.s_addr = INADDR_ANY;
 
     
-	    int n = bind(sock_dscr, (struct sockaddr *)&server, sizeof(server));
-	    if (n < 0)
-	    {
-	    	fprintf(stderr, "ERROR: failed to bind socket\n");
-	    	exit(1);
-	    }
+	    	int n = bind(sock_dscr, (struct sockaddr *)&server, sizeof(server));
+	    	if (n < 0)
+	    	{
+			fprintf(stderr, "ERROR: failed to bind socket\n");
+	    		exit(1);
+	    	}
 
-	    int res = listen(sock_dscr, 1);
-	    if (res != 0)
-	    {
-	    	fprintf(stderr, "ERROR: failed to listen on socket\n");
-	    	exit(1);
-	    }
+	    	int res = listen(sock_dscr, 1);
+	    	if (res != 0)
+	    	{
+			fprintf(stderr, "ERROR: failed to listen on socket\n");
+	    		exit(1);
+	    	}
 
 
-	    ns = accept(sock_dscr, (struct sockaddr *)&client, &namelen);
-	    if (ns == -1)
-	    {
-	    	fprintf(stderr, "ERROR: failed to accept client req\n");
-	    	exit(1);
-	    }
+	    	ns = accept(sock_dscr, (struct sockaddr *)&client, &namelen);
+	    	if (ns == -1)
+	    	{
+			fprintf(stderr, "ERROR: failed to accept client req\n");
+	    		exit(1);
+	    	}
+	    	return ns;
+	}
+	else if (type == CLIENT)
+	{
+		printf("STARTING CLIENT\n");
 
-	    return sock_dscr;
-    }
-    else if (type == CLIENT)
-        {
-            printf("STARTING CLIENT\n");
+	    	int sock_dscr;
+	    	struct sockaddr_in server;
 
-	    int sock_dscr;
-	    struct sockaddr_in server;
+	    	sock_dscr = socket(AF_INET, SOCK_STREAM, 0);
+	    	if (sock_dscr == -1)
+	    	{
+			fprintf(stderr, "ERROR: failed to create a socket\n");
+	    		exit(1);
+	    	}
 
-	    sock_dscr = socket(AF_INET, SOCK_STREAM, 0);
-	    if (sock_dscr == -1)
-	    {
-	    	fprintf(stderr, "ERROR: failed to create a socket\n");
-	    	exit(1);
-	    }
+	    	server.sin_family = AF_INET;
+	    	server.sin_addr.s_addr = inet_addr(ip);
+	    	server.sin_port = htons(port);
 
-	    server.sin_family = AF_INET;
-	    server.sin_addr.s_addr = inet_addr(ip);
-	    server.sin_port = htons(port);
-
-	    int res = connect(sock_dscr, (struct sockaddr *)&server, sizeof(server));
-	    if (res != 0)
-	    {
-	    	fprintf(stderr, "ERROR: failed to connect to server\n");
-	    	exit(1);
-	    }
+	    	int res = connect(sock_dscr, (struct sockaddr *)&server, sizeof(server));
+	    	if (res != 0)
+	    	{
+			fprintf(stderr, "ERROR: failed to connect to server\n");
+	    		exit(1);
+	    	}
     
-	    return sock_dscr;
-    } 
+	    	return sock_dscr;
+	} 
 }
 
 
 void
 listen_tcp
 (
-    int fd,
-    char *buf,
-    size_t buflen
+	int fd,
+    	char *buf,
+    	size_t buflen
 )
 {
-    read(fd, buf, buflen);
+	read(fd, buf, buflen);
 }
 
 void
 send_tcp
 (
-    int fd,
-    char *buf,
-    size_t buflen
+	int fd,
+    	char *buf,
+    	size_t buflen
 )
 {
-    write(fd, buf, buflen);
+	write(fd, buf, buflen);
 }
 
 void
