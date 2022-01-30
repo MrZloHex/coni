1bbd64c6535e0f267aceb0e0369b25512a95798c
MrZloHex <zlo.alex.it@gmail.com>
2022-01-30 16:23:43 +0200

    SERVER

--- src/server.c
+++ src/server.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 
 #include "server.h"
 
@@ -7,7 +9,46 @@
 void
 start_server(uint16_t port)
 {
-	printf("STARTING SERVER\n");
+	printf("STARTING SERVER ON PORT %hu\n", port);
 
+	int sock_dscr = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_dscr == -1)
+	{
+		fprintf(stderr, "ERROR: failed to create a socket\n");
+		exit(1);
+	}
+
+	struct sockaddr_in server;
+	server.sin_family      = AF_INET;
+	server.sin_port        = htons(port);
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	
+	int n = bind(sock_dscr, (struct sockaddr *)&server, sizeof(server));
+	if (n < 0)
+	{
+		fprintf(stderr, "ERROR: failed to bind socket\n");
+		exit(1);
+	}
+
+	int res = listen(sock_dscr, 1);
+	if (res != 0)
+	{
+		fprintf(stderr, "ERROR: failed to listen on socket\n");
+		exit(1);
+	}
+
+	struct sockaddr_in client;
+	socklen_t namelen = sizeof(client);
+
+	int ns = accept(sock_dscr, (struct sockaddr *)&client, &namelen);
+	if (ns == -1)
+	{
+		fprintf(stderr, "ERROR: failed to accept client req\n");
+		exit(1);
+	}
+
+
+	close(sock_dscr);
 }
 
